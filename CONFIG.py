336228bdaa208fbf76fa6703082caaac6c5c6cc6
hardlydavis <davidcharvey@googlemail.com>
2017-06-20 11:46:28 +0100

    Update CONFIG.py
    fixed typos

--- CONFIG.py
+++ CONFIG.py
@@ -3,7 +3,7 @@ class CONFIG():
   BAMBOO_URL = 'https://api.bamboohr.com/api/gateway.php/{yourbamboosubdomain}/v1/employees'
   BAMBOO_API_KEY = "{yourAPIkey}"
   IPA_SERVER = "ipaserver.yourdomain.com"
-  # Example bind detailsw for coneventional freeipa account (requires user admin rights)
+  # Example bind details for conventional freeipa account granted user admin role
   BIND_DN = 'uid=bamboosync,cn=users,cn=accounts,dc=example,dc=net'
   BIND_PW = ''
   BAMBOO_EXCLUDE_LIST = "somevalue"
